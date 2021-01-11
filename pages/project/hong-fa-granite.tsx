3d07ce0f01d143855b030b3bc5f6ce7df6d8771f
Fullchee Zhang <fullchee@gmail.com>
2021-01-11 12:21:23 -0500

    Add more content for hong-fa-granite

--- pages/project/hong-fa-granite.tsx
+++ pages/project/hong-fa-granite.tsx
@@ -1,6 +1,7 @@
 import Layout from "../../components/layout";
 import Link from "next/link";
 import { ProjectOverview } from "../../components/project/ProjectOverview";
+import Image from "next/image";
 
 const HongFaGranitePage = () => {
   const skills = [
@@ -51,21 +52,20 @@ collaboration?  */}
 way?  */}
       {/* When you did get stuck, how did you resolve it? How did you overcome the obstacles
 you faced?  */}
+      <h3>What kind of site is best for the client</h3>
       <p>
-        My first course of action was to see if building a Gatsby site was the
-        right thing to do for the client. I listen to a ton of podcasts and
-        watch a ton of "educational" (fun to learn but not really useful)
-        YouTube and I always see Squarespace and Wix ads.
+        The first thing to do was determining if building a Gatsby site was the
+        right thing to do for the client. Or if a website builder meets their
+        needs better.
       </p>
       <p>
-        It turns out that they're really expensive (compared to my free labour).
-        As of January 2021, Squarespace's cheapest plan is{" "}
+        It turns out that website builders really expensive (compared to my free
+        labour). As of January 2021, Squarespace's cheapest plan is{" "}
         <a href="https://www.squarespace.com/pricing">
           $12 USD/month = $200+ CAD/year
         </a>
         . Wix isn't that much cheaper at $10 USD/month for their cheapest plan.
-        None of these plans come with email long-term which is the real cost and
-        switching email providers can be quite difficult.
+        None of these plans come with email long-term which can get pricey.
       </p>
 
       <p>
@@ -73,10 +73,46 @@ you faced?  */}
         I had to design it. I asked my client a lot of questions to get a sense
         of what kind of brand Hong Fa Granite was.
         <ul>
-          <li></li>
-          <li></li>
+          <li>What the company does</li>
+          <li>the company's selling point</li>
+          <li>how we want the customers to perceive the company</li>
+          <li>the target audience (user personas)</li>
         </ul>
-        , My go to strategy was sending my client logos. After they picked one,
+      </p>
+      <h3>Logo design</h3>
+      <h4>Old logo</h4>
+      <Image
+        src="/assets/images/projects/hong-fa-granite/old-logo.png"
+        alt="Old Hong Fa Granite logo"
+        width={300}
+        height={84}
+      />
+      <p>Logo design plan</p>
+      <ol>
+        <li>Decide on a serif font for the logo</li>
+        <li>Update the logo to be favicon friendly</li>
+      </ol>
+
+      <h4>New logo</h4>
+      <Image
+        src="/assets/images/projects/hong-fa-granite/new-logo.png"
+        alt="New Hong Fa Granite logo"
+        width={313}
+        height={107}
+      />
+
+      <p>
+        I went through serif fonts on Google Fonts to find a font that the
+        client liked. I limited it to serif fonts to convey a classic style to
+        reflect the classic nature of stone counter tops. Similarly, I went
+        through logos of stone related companies and I decided on a geometric
+        design with an H in the middle. I kept the brown color scheme of the old
+        logo
+      </p>
+      <h3>Site design</h3>
+      <p>
+        The Gatsby theme I started with came with the Bulma CSS framework so I
+        used Bulma components.
       </p>
       <p>
         Google Maps integration was fairly straightforward because of existing
@@ -86,9 +122,16 @@ you faced?  */}
           <a>Batch calendar events project.</a>
         </Link>
       </p>
+      <h3>Deploying</h3>
+      <p>
+        I deployed to a free Netlify site to get feedback from the client. Once
+        the site was ready for deployment.
+      </p>
       <p>
         Switching from Bell Hosting to Netlify was predictably the hardest and
-        least fun part of this project.
+        least fun part of this project. Eventually, I switched to another domain
+        provider and connected it with Netlify so that HongFaGranite.com was
+        live!
       </p>
       <h2>Lessons learned</h2>
       {/* What did you learn from doing this project? How has it affected the work you’ve done
