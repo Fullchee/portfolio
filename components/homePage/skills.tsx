c9b56de90ddccad051b75872a1dcaa771c8d5624
Fullchee Zhang <fullchee@gmail.com>
2021-08-03 21:22:59 -0400

    p tag cannot be a parent of a ul tag

--- components/homePage/skills.tsx
+++ components/homePage/skills.tsx
@@ -1,15 +1,15 @@
-import { SkillList } from "../common/SkillList";
+import { SkillList } from '../common/SkillList';
 
 const Skills = () => {
   const skills = [
-    { icon: "js", label: "JavaScript ES6+" },
-    { icon: "html5", label: "HTML & CSS" },
-    { icon: "react", label: "React" },
-    { icon: "sass", label: "LESS and SASS" },
-    { icon: "code-branch", label: "Git" },
-    { icon: "database", label: "SQL" },
-    { icon: "node-js", label: "Node.js" },
-    { icon: "typescript-icon.svg", label: "TypeScript" },
+    { icon: 'js', label: 'JavaScript ES6+' },
+    { icon: 'html5', label: 'HTML & CSS' },
+    { icon: 'react', label: 'React' },
+    { icon: 'sass', label: 'LESS and SASS' },
+    { icon: 'code-branch', label: 'Git' },
+    { icon: 'database', label: 'SQL' },
+    { icon: 'node-js', label: 'Node.js' },
+    { icon: 'typescript-icon.svg', label: 'TypeScript' },
   ];
   return (
     <section className="flex-col flex flex-wrap mb-8">
@@ -19,24 +19,21 @@ const Skills = () => {
       <ul className="flex flex-wrap skills no-list-item">
         <SkillList skills={skills} />
       </ul>
-      <p className="pt-10">
-        I learn and stay up to date by
-        <ul>
-          <li>
-            listening to the <a href="https://syntax.fm/">Syntax podcast</a>
-          </li>
-          <li>
-            <a href="https://zerotomastery.io/blog/web-developer-monthly-dec-2020/">
-              ZTM web dev monthly newsletter
-            </a>
-          </li>
-          <li>working on open source side projects</li>
-          <li>
-            retaining what I learn with{" "}
-            <a href="https://apps.ankiweb.net/">Anki</a>
-          </li>
-        </ul>
-      </p>
+      <p className="pt-10">I learn and stay up to date by</p>
+      <ul>
+        <li>
+          listening to the <a href="https://syntax.fm/">Syntax podcast</a>
+        </li>
+        <li>
+          <a href="https://zerotomastery.io/blog/web-developer-monthly-dec-2020/">
+            ZTM web dev monthly newsletter
+          </a>
+        </li>
+        <li>working on open source side projects</li>
+        <li>
+          retaining what I learn with <a href="https://apps.ankiweb.net/">Anki</a>
+        </li>
+      </ul>
     </section>
   );
 };
