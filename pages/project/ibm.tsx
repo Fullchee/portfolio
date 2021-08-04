ec9640684428b39351c8d356154c82299dd24b76
Fullchee Zhang <fullchee@gmail.com>
2021-08-03 21:23:58 -0400

    Move ProjectDescription to it's own component

--- pages/project/ibm.tsx
+++ pages/project/ibm.tsx
@@ -1,32 +1,7 @@
-import { ReactNode } from 'react';
 import Link from 'next/link';
 
+import { ProjectDescription } from './ProjectDescription';
 import Layout from '../../components/layout';
-import Image from 'next/image';
-
-type Image = {
-  src: string;
-  alt: string;
-  width: number;
-  height: number;
-  onRight?: boolean;
-};
-
-type DescriptionProps = {
-  children?: ReactNode;
-  image: Image;
-};
-
-const ProjectDescription = ({ children, image }: DescriptionProps) => {
-  return (
-    <div className="grid sm:grid-cols-2 sm:grid-rows-1 items-center py-12">
-      <div className={`${image.onRight ? 'order-1 justify-self-end' : ''}`}>
-        <Image src={image.src} width={image.width} height={image.height} alt={image.alt}></Image>
-      </div>
-      <div className={`${image.onRight ? '' : ''}`}>{children}</div>
-    </div>
-  );
-};
 
 const IBMPage = () => {
   return (
---
+++ pages/project/utilities.ts
@@ -0,0 +1,15 @@
+export const calculateSejour = (date: Date) => {
+  const interval = new Date().getTime() - new Date(date).getTime();
+  const days = Math.floor(interval / (1000 * 60 * 60 * 24));
+  const months = Math.round((days % 365) / 30);
+  const monthsString = `${months} month` + months ? 's' : '';
+  const years = Math.floor(days / 365);
+  switch (years) {
+    case 0:
+      return monthsString;
+    case 1:
+      return '1 year and ' + monthsString;
+    default:
+      return `${years} years and ${monthsString}`;
+  }
+};
