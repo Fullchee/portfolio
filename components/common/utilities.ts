fe3d43db2adf8572e6a850f06ba49765c35ddd26
Fullchee Zhang <fullchee@gmail.com>
2022-04-16 15:01:47 -0400

    Fix logic bug on forma page

--- components/common/utilities.ts
+++ components/common/utilities.ts
@@ -1,15 +1,14 @@
 export const calculateSejour = (date: string) => {
   const interval = new Date().getTime() - new Date(date).getTime();
   const days = Math.floor(interval / (1000 * 60 * 60 * 24));
-  console.log(days);
-  const months = Math.round((156 % 365) / 30);
-  const monthsString = `${months} month${months ? 's' : ''}`;
+  const months = Math.round((days % 365) / 30);
+  const monthsString = `${months} month${months ? "s" : ""}`;
   const years = Math.floor(days / 365);
   switch (years) {
     case 0:
       return monthsString;
     case 1:
-      return '1 year and ' + monthsString;
+      return "1 year and " + monthsString;
     default:
       return `${years} years and ${monthsString}`;
   }
