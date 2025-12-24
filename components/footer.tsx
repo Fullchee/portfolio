c3722c3b412073b01c1cbae1081c8703b078358f
Fullchee Zhang <fullchee@gmail.com>
2025-12-23 21:05:37 -0500

    [skip ci] Fix linkedin resume

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,41 +1,41 @@
 import Link from "next/link";
 
 const Footer = () => {
-  return (
-    <footer className="py-12 bg-accent-1 border-t border-accent-2 mt-16">
-      <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0">
-        <div className="flex flex-col md:flex-row justify-center items-center md:pl-4 md:w-1/2 space-y-4 md:space-y-0">
-          <a
-            href={`https://github.com/fullchee`}
-            className="no-underline mx-3 font-bold"
-          >
-            GitHub
-          </a>
-          <a
-            href="https://www.linkedin.com/in/fullchee-zhang"
-            className="no-underline mx-3 font-bold"
-          >
-            LinkedIn
-          </a>
-          <Link
-            href="/assets/Fullchee-Resume.pdf"
-            className="no-underline mx-3 font-bold"
-          >
-            Resume
-          </Link>
-        </div>
+	return (
+		<footer className="py-12 bg-accent-1 border-t border-accent-2 mt-16">
+			<div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0">
+				<div className="flex flex-col md:flex-row justify-center items-center md:pl-4 md:w-1/2 space-y-4 md:space-y-0">
+					<a
+						href={`https://github.com/fullchee`}
+						className="no-underline mx-3 font-bold"
+					>
+						GitHub
+					</a>
+					<a
+						href="https://www.linkedin.com/in/fullchee"
+						className="no-underline mx-3 font-bold"
+					>
+						LinkedIn
+					</a>
+					<Link
+						href="/assets/Fullchee-Resume.pdf"
+						className="no-underline mx-3 font-bold"
+					>
+						Resume
+					</Link>
+				</div>
 
-        <p className="text-xs text-center">
-          Made with{" "}
-          <Link href="/attributions" className="text-center">
-            Creative Commons Resources
-          </Link>
-          , <a href="https://nextjs.org/">Next.js</a>,{" "}
-          <a href="https://tailwindcss.com/">Tailwind.css</a> and love ❤️
-        </p>
-      </div>
-    </footer>
-  );
+				<p className="text-xs text-center">
+					Made with{" "}
+					<Link href="/attributions" className="text-center">
+						Creative Commons Resources
+					</Link>
+					, <a href="https://nextjs.org/">Next.js</a>,{" "}
+					<a href="https://tailwindcss.com/">Tailwind.css</a> and love ❤️
+				</p>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
