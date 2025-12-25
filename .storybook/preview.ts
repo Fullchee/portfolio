022b8975cfa6eb36898185638a78b759ba52a979
Fullchee Zhang <fullchee@gmail.com>
2025-12-25 10:48:55 -0500

    [skip ci] Add tailwind to storybook config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,10 @@
 import addonA11y from "@storybook/addon-a11y";
 import { definePreview } from "@storybook/nextjs-vite";
+import "../styles/globals.css";
 
 export default definePreview({
-	// 👇 Add your addons here
 	addons: [addonA11y()],
 	parameters: {
-		// type-safe!
 		a11y: {
 			options: { xpath: true },
 		},
